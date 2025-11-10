b29d9ecbd1620ffe2f9dc797658a4de8e9248dfd
oppiKlop <157105767+oppiKlop@users.noreply.github.com>
2025-11-11 00:01:00 +0300

    Update Email.java

--- src/main/java/com/mipt/mikhailandrosov/Email.java
+++ src/main/java/com/mipt/mikhailandrosov/Email.java
@@ -1,1 +1,9 @@
+package com.mipt.mikhailandrosov;
 
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Email {
+  String message() default "Invalid email format";
+}
